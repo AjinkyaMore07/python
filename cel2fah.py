583f8982839c67ad6ecd4f69a673434702f18d46
Ajinkya Bhausaheb More <113751745+AjinkyaMore07@users.noreply.github.com>
2024-05-30 22:01:58 +0530

    program for celcius to fahrenheit

---
+++ cel2fah.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+#Temperature in degrees Fahrenheit (°F) = (Temperature in degrees Celsius (°C) * 9/5) + 32.
+A = int(input("enter temp in celcius"))
+result = (A*(9/5)+32)
+print(" F  = ",result)
+
+

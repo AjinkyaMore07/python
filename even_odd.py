dd48c549353a02e2085588b331183d5b23795339
Ajinkya Bhausaheb More <113751745+AjinkyaMore07@users.noreply.github.com>
2024-05-30 22:16:19 +0530

    Even or odd

---
+++ even_odd.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+A = int(input(" enter number to find it is even or odd "))
+if (A ==0):
+    print(" 0 is not even nor odd ")
+elif (A%2 == 0):
+    print("even")
+else:
+    print("odd")
